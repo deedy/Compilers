ed3653a7cfb44536e750e7a9dcb79f4b3c910939
dtwitty <dktwitty@gmail.com>
2013-09-05 21:41:20 +0000

    bugfix in the xiprog

--- ANTLR/Cubex/CubeXProg.java
+++ ANTLR/Cubex/CubeXProg.java
@@ -4,22 +4,22 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 
-public class XiProg {
+public class CubexProg {
     public static void main(String[] args) throws Exception {
     	// lex the file
-        XiLexer lex = new XiLexer(new ANTLRFileStream(args[0]));
+        XiLexer lex = new CubexLexer(new ANTLRFileStream(args[0]));
         // make a token stream (may be unnecessary for now)
-        CommonTokenStream tokens = new CommonTokenStream(lex);
+        CommonTokenStream tokens = new CommonTokenStream(lex));
 		// consume next token until EOF
 		for (Token token = lex.nextToken();
 		     token.getType() != Token.EOF;
 		     token = lex.nextToken()){
-
+			
         	int type = token.getType();
         	if(type > 0){
-        		System.out.print(lex.ruleNames[token.getType()]);
+        		// we have a valid type
+
         	} 
-        	System.out.printf(" %s\n", token.getText());
         }
     }
 }
