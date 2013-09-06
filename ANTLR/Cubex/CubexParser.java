911d027cc18a28a9efd059e2a87127e191068c96
Simon Li <cl585@cornell.edu>
2013-09-05 23:40:55 -0400

    Added nested comments

--- ANTLR/Cubex/CubexParser.java
+++ ANTLR/Cubex/CubexParser.java
@@ -14,18 +14,19 @@ public class CubexParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		RANGEOP=25, MODULO=11, CLASS=2, LT=14, RBRACE=34, WHILE=6, PLUSPLUS=26, 
-		LBRACE=33, ELSE=8, GTE=17, BOOL=39, NEGATE=22, INT=38, SEMICOLON=32, MINUS=13, 
-		AND=20, LTE=16, LPAREN=29, TYPE=35, IF=7, COLON=27, RPAREN=30, NAME=36, 
-		WS=37, IN=9, COMMA=31, EQUAL=28, OR=21, RETURN=10, GT=15, PLUS=12, INTERFACE=1, 
-		SUPER=4, EQ=18, COMMENT=40, FUN=5, TIMES=23, EXTENDS=3, DIVIDE=24, STRING=41, 
-		NE=19;
+		RANGEOP=25, MODULO=11, CLASS=2, LT=14, RBRACE=34, WHILE=6, MULTILINECOMMENT=44, 
+		PLUSPLUS=26, LBRACE=33, ELSE=8, GTE=17, BOOL=40, NEGATE=22, INT=39, SEMICOLON=32, 
+		MINUS=13, AND=20, SINGLELINECOMMENT=43, LTE=16, LPAREN=29, TYPE=36, IF=7, 
+		COLON=27, RPAREN=30, NAME=37, WS=38, IN=9, COMMA=31, EQUAL=28, OR=21, 
+		RETURN=10, GT=15, PLUS=12, INTERFACE=1, SUPER=4, EQ=18, COMMENT=41, DOT=35, 
+		FUN=5, TIMES=23, EXTENDS=3, DIVIDE=24, STRING=42, NE=19;
 	public static final String[] tokenNames = {
 		"<INVALID>", "'interface'", "'class'", "'extends'", "'super'", "'fun'", 
 		"'while'", "'if'", "'else'", "'in'", "'return'", "'%'", "'+'", "'-'", 
 		"'<'", "'>'", "'<='", "'>='", "'=='", "'!='", "'&'", "'|'", "'!'", "'*'", 
 		"'/'", "RANGEOP", "'++'", "':'", "'='", "'('", "')'", "','", "';'", "'{'", 
-		"'}'", "TYPE", "NAME", "WS", "INT", "BOOL", "COMMENT", "STRING"
+		"'}'", "'.'", "TYPE", "NAME", "WS", "INT", "BOOL", "COMMENT", "STRING", 
+		"SINGLELINECOMMENT", "MULTILINECOMMENT"
 	};
 	public static final int
 		RULE_string = 0, RULE_comment = 1, RULE_type = 2, RULE_name = 3, RULE_bool = 4, 
@@ -293,26 +294,27 @@ public class CubexParser extends Parser {
 
 	public static class SymbolContext extends ParserRuleContext {
 		public TerminalNode LBRACE() { return getToken(CubexParser.LBRACE, 0); }
-		public TerminalNode SEMICOLON() { return getToken(CubexParser.SEMICOLON, 0); }
-		public TerminalNode GTE() { return getToken(CubexParser.GTE, 0); }
-		public TerminalNode TIMES() { return getToken(CubexParser.TIMES, 0); }
-		public TerminalNode MODULO() { return getToken(CubexParser.MODULO, 0); }
 		public TerminalNode AND() { return getToken(CubexParser.AND, 0); }
-		public TerminalNode MINUS() { return getToken(CubexParser.MINUS, 0); }
-		public TerminalNode LTE() { return getToken(CubexParser.LTE, 0); }
-		public TerminalNode OR() { return getToken(CubexParser.OR, 0); }
 		public TerminalNode PLUSPLUS() { return getToken(CubexParser.PLUSPLUS, 0); }
-		public TerminalNode EQUAL() { return getToken(CubexParser.EQUAL, 0); }
-		public TerminalNode RBRACE() { return getToken(CubexParser.RBRACE, 0); }
-		public TerminalNode LPAREN() { return getToken(CubexParser.LPAREN, 0); }
 		public TerminalNode DIVIDE() { return getToken(CubexParser.DIVIDE, 0); }
 		public TerminalNode EQ() { return getToken(CubexParser.EQ, 0); }
+		public TerminalNode PLUS() { return getToken(CubexParser.PLUS, 0); }
+		public TerminalNode TIMES() { return getToken(CubexParser.TIMES, 0); }
+		public TerminalNode GTE() { return getToken(CubexParser.GTE, 0); }
+		public TerminalNode SEMICOLON() { return getToken(CubexParser.SEMICOLON, 0); }
+		public TerminalNode MODULO() { return getToken(CubexParser.MODULO, 0); }
+		public TerminalNode OR() { return getToken(CubexParser.OR, 0); }
+		public TerminalNode LTE() { return getToken(CubexParser.LTE, 0); }
+		public TerminalNode MINUS() { return getToken(CubexParser.MINUS, 0); }
+		public TerminalNode EQUAL() { return getToken(CubexParser.EQUAL, 0); }
+		public TerminalNode LPAREN() { return getToken(CubexParser.LPAREN, 0); }
+		public TerminalNode RBRACE() { return getToken(CubexParser.RBRACE, 0); }
+		public TerminalNode DOT() { return getToken(CubexParser.DOT, 0); }
 		public TerminalNode LT() { return getToken(CubexParser.LT, 0); }
 		public TerminalNode COMMA() { return getToken(CubexParser.COMMA, 0); }
 		public TerminalNode RPAREN() { return getToken(CubexParser.RPAREN, 0); }
 		public TerminalNode NE() { return getToken(CubexParser.NE, 0); }
 		public TerminalNode RANGEOP() { return getToken(CubexParser.RANGEOP, 0); }
-		public TerminalNode PLUS() { return getToken(CubexParser.PLUS, 0); }
 		public TerminalNode GT() { return getToken(CubexParser.GT, 0); }
 		public TerminalNode COLON() { return getToken(CubexParser.COLON, 0); }
 		public TerminalNode NEGATE() { return getToken(CubexParser.NEGATE, 0); }
@@ -331,7 +333,7 @@ public class CubexParser extends Parser {
 			{
 			setState(38);
 			_la = _input.LA(1);
-			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << MODULO) | (1L << PLUS) | (1L << MINUS) | (1L << LT) | (1L << GT) | (1L << LTE) | (1L << GTE) | (1L << EQ) | (1L << NE) | (1L << AND) | (1L << OR) | (1L << NEGATE) | (1L << TIMES) | (1L << DIVIDE) | (1L << RANGEOP) | (1L << PLUSPLUS) | (1L << COLON) | (1L << EQUAL) | (1L << LPAREN) | (1L << RPAREN) | (1L << COMMA) | (1L << SEMICOLON) | (1L << LBRACE) | (1L << RBRACE))) != 0)) ) {
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << MODULO) | (1L << PLUS) | (1L << MINUS) | (1L << LT) | (1L << GT) | (1L << LTE) | (1L << GTE) | (1L << EQ) | (1L << NE) | (1L << AND) | (1L << OR) | (1L << NEGATE) | (1L << TIMES) | (1L << DIVIDE) | (1L << RANGEOP) | (1L << PLUSPLUS) | (1L << COLON) | (1L << EQUAL) | (1L << LPAREN) | (1L << RPAREN) | (1L << COMMA) | (1L << SEMICOLON) | (1L << LBRACE) | (1L << RBRACE) | (1L << DOT))) != 0)) ) {
 			_errHandler.recoverInline(this);
 			}
 			consume();
@@ -407,6 +409,7 @@ public class CubexParser extends Parser {
 			case SEMICOLON:
 			case LBRACE:
 			case RBRACE:
+			case DOT:
 				{
 				setState(41); symbol();
 				}
@@ -492,7 +495,7 @@ public class CubexParser extends Parser {
 			setState(54);
 			_errHandler.sync(this);
 			_la = _input.LA(1);
-			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << INTERFACE) | (1L << CLASS) | (1L << EXTENDS) | (1L << SUPER) | (1L << FUN) | (1L << WHILE) | (1L << IF) | (1L << ELSE) | (1L << IN) | (1L << RETURN) | (1L << MODULO) | (1L << PLUS) | (1L << MINUS) | (1L << LT) | (1L << GT) | (1L << LTE) | (1L << GTE) | (1L << EQ) | (1L << NE) | (1L << AND) | (1L << OR) | (1L << NEGATE) | (1L << TIMES) | (1L << DIVIDE) | (1L << RANGEOP) | (1L << PLUSPLUS) | (1L << COLON) | (1L << EQUAL) | (1L << LPAREN) | (1L << RPAREN) | (1L << COMMA) | (1L << SEMICOLON) | (1L << LBRACE) | (1L << RBRACE) | (1L << TYPE) | (1L << NAME) | (1L << WS) | (1L << INT) | (1L << BOOL) | (1L << COMMENT) | (1L << STRING))) != 0)) {
+			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << INTERFACE) | (1L << CLASS) | (1L << EXTENDS) | (1L << SUPER) | (1L << FUN) | (1L << WHILE) | (1L << IF) | (1L << ELSE) | (1L << IN) | (1L << RETURN) | (1L << MODULO) | (1L << PLUS) | (1L << MINUS) | (1L << LT) | (1L << GT) | (1L << LTE) | (1L << GTE) | (1L << EQ) | (1L << NE) | (1L << AND) | (1L << OR) | (1L << NEGATE) | (1L << TIMES) | (1L << DIVIDE) | (1L << RANGEOP) | (1L << PLUSPLUS) | (1L << COLON) | (1L << EQUAL) | (1L << LPAREN) | (1L << RPAREN) | (1L << COMMA) | (1L << SEMICOLON) | (1L << LBRACE) | (1L << RBRACE) | (1L << DOT) | (1L << TYPE) | (1L << NAME) | (1L << WS) | (1L << INT) | (1L << BOOL) | (1L << COMMENT) | (1L << STRING))) != 0)) {
 				{
 				setState(52);
 				switch (_input.LA(1)) {
@@ -565,6 +568,7 @@ public class CubexParser extends Parser {
 				case SEMICOLON:
 				case LBRACE:
 				case RBRACE:
+				case DOT:
 					{
 					setState(51); other();
 					}
@@ -591,18 +595,18 @@ public class CubexParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\uacf5\uee8c\u4f5d\u8b0d\u4a45\u78bd\u1b2f\u3378\3+<\4\2\t\2\4\3\t\3"+
+		"\3\uacf5\uee8c\u4f5d\u8b0d\u4a45\u78bd\u1b2f\u3378\3.<\4\2\t\2\4\3\t\3"+
 		"\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f"+
 		"\t\f\3\2\3\2\3\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3"+
 		"\n\3\n\3\13\3\13\5\13-\n\13\3\f\3\f\3\f\3\f\3\f\3\f\3\f\3\f\7\f\67\n\f"+
-		"\f\f\16\f:\13\f\3\f\2\r\2\4\6\b\n\f\16\20\22\24\26\2\4\3\2\3\f\3\2\r$"+
+		"\f\f\16\f:\13\f\3\f\2\r\2\4\6\b\n\f\16\20\22\24\26\2\4\3\2\3\f\3\2\r%"+
 		"9\2\30\3\2\2\2\4\32\3\2\2\2\6\34\3\2\2\2\b\36\3\2\2\2\n \3\2\2\2\f\"\3"+
 		"\2\2\2\16$\3\2\2\2\20&\3\2\2\2\22(\3\2\2\2\24,\3\2\2\2\268\3\2\2\2\30"+
-		"\31\7+\2\2\31\3\3\2\2\2\32\33\7*\2\2\33\5\3\2\2\2\34\35\7%\2\2\35\7\3"+
-		"\2\2\2\36\37\7&\2\2\37\t\3\2\2\2 !\7)\2\2!\13\3\2\2\2\"#\7(\2\2#\r\3\2"+
-		"\2\2$%\7\'\2\2%\17\3\2\2\2&\'\t\2\2\2\'\21\3\2\2\2()\t\3\2\2)\23\3\2\2"+
-		"\2*-\5\20\t\2+-\5\22\n\2,*\3\2\2\2,+\3\2\2\2-\25\3\2\2\2.\67\5\2\2\2/"+
-		"\67\5\4\3\2\60\67\5\6\4\2\61\67\5\b\5\2\62\67\5\n\6\2\63\67\5\f\7\2\64"+
+		"\31\7,\2\2\31\3\3\2\2\2\32\33\7+\2\2\33\5\3\2\2\2\34\35\7&\2\2\35\7\3"+
+		"\2\2\2\36\37\7\'\2\2\37\t\3\2\2\2 !\7*\2\2!\13\3\2\2\2\"#\7)\2\2#\r\3"+
+		"\2\2\2$%\7(\2\2%\17\3\2\2\2&\'\t\2\2\2\'\21\3\2\2\2()\t\3\2\2)\23\3\2"+
+		"\2\2*-\5\20\t\2+-\5\22\n\2,*\3\2\2\2,+\3\2\2\2-\25\3\2\2\2.\67\5\2\2\2"+
+		"/\67\5\4\3\2\60\67\5\6\4\2\61\67\5\b\5\2\62\67\5\n\6\2\63\67\5\f\7\2\64"+
 		"\67\5\16\b\2\65\67\5\24\13\2\66.\3\2\2\2\66/\3\2\2\2\66\60\3\2\2\2\66"+
 		"\61\3\2\2\2\66\62\3\2\2\2\66\63\3\2\2\2\66\64\3\2\2\2\66\65\3\2\2\2\67"+
 		":\3\2\2\28\66\3\2\2\289\3\2\2\29\27\3\2\2\2:8\3\2\2\2\5,\668";
