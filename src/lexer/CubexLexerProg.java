3b548e3f4cdf63f4185be02bef253145f35f1250
dtwitty <dktwitty@gmail.com>
2013-09-07 19:02:01 +0000

    major refactoring

--- src/lexer/CubexLexerProg.java
+++ src/lexer/CubexLexerProg.java
@@ -4,22 +4,22 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 
-public class CubexProg {
+public class CubexLexerProg {
     public static void main(String[] args) throws Exception {
     	// lex the file
-        XiLexer lex = new CubexLexer(new ANTLRFileStream(args[0]));
+        CubexLexer lex = new CubexLexer(new ANTLRFileStream(args[0]));
         // make a token stream (may be unnecessary for now)
-        CommonTokenStream tokens = new CommonTokenStream(lex));
+        CommonTokenStream tokens = new CommonTokenStream(lex);
 		// consume next token until EOF
 		for (Token token = lex.nextToken();
 		     token.getType() != Token.EOF;
 		     token = lex.nextToken()){
 			
-        	int type = token.getType();
-        	if(type > 0){
-        		// we have a valid type
-
-        	} 
+            int type = token.getType();
+            if(type > 0){
+                System.out.print(lex.ruleNames[token.getType()]);
+            } 
+            System.out.printf(" %s\n", token.getText());
         }
     }
 }
