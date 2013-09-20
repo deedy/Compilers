f226e8cb1f32757cd4f6049db3221b6f09d75d1e
dtwitty <dktwitty@gmail.com>
2013-09-20 16:54:55 +0000

    added funciton sig

---
+++ src/parser/CubexTypeScheme.java
@@ -0,0 +1,6 @@
+// essentially a function signature, 
+// maps type params and function params to a type
+public class CubexTypeScheme {
+	public CubexTypeScheme(CubexKindContext k, CubexTypeContext t){
+	}
+}
