c21ec7952660d389807751076b5fc76d86d39df6
bae43 <bae43@cornell.edu>
2013-10-27 21:19:50 -0400

    added method stubs for visit(). :D

--- src/generator/HVisitor.java
+++ src/generator/HVisitor.java
@@ -1,5 +1,129 @@
 public class HVisitor {
-    HNode visit(Object n) {
+
+    HNode visit(CubexFunctionCall n) {
         return null;
     }
+
+    HNode visit(CubexVar n) {
+        return null;
+    }
+
+    HNode visit(CubexMethodCall n) {
+        return null;
+    }
+
+    HNode visit(CubexAppend n) {
+        return null;
+    }
+
+    HNode visit(CubexIterable n) {
+        return null;
+    }
+
+    HNode visit(CubexBoolean n) {
+        return null;
+    }
+
+    HNode visit(CubexInt n) {
+        return null;
+    }
+
+    HNode visit(CubexString n) {
+        return null;
+    }
+
+    HNode visit(CubexFunHeader n) {
+        return null;
+    }
+
+    HNode visit(CubexName n) {
+        return null;
+    }
+
+    HNode visit(Nothing n) {
+        return null;
+    }
+
+    HNode visit(Thing n) {
+        return null;
+    }
+    
+    HNode visit(CubexPType n) {
+        return null;
+    }
+    
+    HNode visit(CubexCType n) {
+        return null;
+    }
+    
+    HNode visit(CubexIType n) {
+        return null;
+    }
+    
+    HNode visit(CubexBlock n) {
+        return null;
+    }
+    
+    HNode visit(CubexAssign n) {
+        return null;
+    }
+    
+    HNode visit(CubexConditional n) {
+        return null;
+    }
+    
+    HNode visit(CubexWhileLoop n) {
+        return null;
+    }
+    
+    HNode visit(CubexForLoop n) {
+        return null;
+    }
+    
+    HNode visit(CubexReturn n) {
+        return null;
+    }
+    
+    HNode visit(CubexInterface n) {
+        return null;
+    }
+    
+    HNode visit(CubexClass n) {
+        return null;
+    }
+    
+    HNode visit(CubexFunction n) {
+        return null;
+    }
+    
+    HNode visit(CubexClassProg n) {
+        return null;
+    }
+    
+    HNode visit(CubexInterfaceProg n) {
+        return null;
+    }
+
+    HNode visit(CubexFuncsProg n) {
+        return null;
+    }
+    
+    HNode visit(CubexStatementsProg n) {
+        return null;
+    }
+    
+    HNode visit(CubexStatementProg n) {
+        return null;
+    }
+    
+    HNode visit(CubexParserProg n) {
+        return null;
+    }
+    
 }
+
+
+
+
+
+
