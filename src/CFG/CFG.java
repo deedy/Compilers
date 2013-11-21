166aa5fae0ea61c6f3d6950337650eaa6ff7e82c
dtwitty <dktwitty@gmail.com>
2013-11-21 01:25:59 -0500

    construction of CFG nearing completion

--- src/CFG/CFG.java
+++ src/CFG/CFG.java
@@ -3,8 +3,29 @@ import org.pcollections.HashTreePSet;
 import org.pcollections.MapPSet;
 
 // some typedefs
-class NodeList extends ArrayList<CFGNode> {}
+class NodeList extends ArrayList<CFGNode> {
+	public NodeList (Collection<CFGNode> coll) {
+		super();
+		for (CFGNode c : coll) {
+			add(c);
+		}
+	}
+
+	public NodeList (CFGNode n) {
+		super();
+		add(n);
+	}
+
+	public NodeList () {
+		super();
+	}
+}
 class StmtList extends ArrayList<LStmt> {}
+class NodeListPair extends Pair<NodeList, NodeList> {
+	public NodeListPair (NodeList l1, NodeList l2) {
+		super(l1, l2);
+	}
+}
 
 abstract class CFGNode {
 	// the original statement
@@ -24,8 +45,10 @@ abstract class CFGNode {
 	// used to rebuild the IR
 	NodeList children = new NodeList();
 
-	// construct the CFG from the semi-converted IR tree
-	public abstract void setEdges(NodeList incoming, NodeList outgoing);
+
+	// makes all internal connections
+	// then exposes all entries and exits of the subgraph
+	public abstract NodeListPair buildSubGraph();
 
 	// build an IR Tree with garbage collection
 	public abstract StmtList translate ();
@@ -65,11 +88,13 @@ class CFGStmts extends CFGNode {
 		}
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
 		int len = children.size();
+		NodeList entering = new NodeList();
+		NodeList exiting = new NodeList();
 		if (len > 0) {
-			children.get(0).prev.addAll(incoming);
-			children.get(len - 1).succ.addAll(outgoing);
+			entering.add(children.get(0));
+			exiting.add(children.get(len - 1));
 			for (int i = 1; i < len; i++) {
 				CFGNode a = children.get(i - 1);
 				CFGNode b = children.get(i);
@@ -77,22 +102,43 @@ class CFGStmts extends CFGNode {
 				b.prev.add(a);
 			}
 		}
+		return new NodeListPair(entering, exiting);
 	}
 
-	public StmtList translate() {
+	public StmtList translate () {
 		return null;
 	}
 }
 
 class CFGFor extends CFGNode {
 	LFor stmt;
+	CFGBranch br;
 
 	public CFGFor (LFor s,  CFG graph) {
 		stmt = s;
+		CFGNode c = stmt.stmt.toCFGNode(graph);
+		children.add(c);
+		br = new CFGBranch(graph);
+		br.use = stmt.iter.getNames();
+		br.def = stmt.elem.getNames();
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
+		CFGNode c = children.get(0);
+		NodeListPair pair = c.buildSubGraph();
+		NodeList entering = new NodeList(br);
+		NodeList exiting = new NodeList(pair.getRight());
+		br.succ.addAll(entering);
+		for (CFGNode i : entering) {
+				i.prev.add(br);
+			for (CFGNode j : exiting) {
+				j.succ.add(i);
+				i.prev.add(j);
+			}
+		}
+		return new NodeListPair(entering, exiting);
 	}
+
 
 	public StmtList translate() {
 		return null;
@@ -101,12 +147,30 @@ class CFGFor extends CFGNode {
 
 class CFGWhile extends CFGNode {
 	LWhile stmt;
+	CFGBranch br;
 
 	public CFGWhile (LWhile s,  CFG graph) {
 		stmt = s;
+		CFGNode c = stmt.stmt.toCFGNode(graph);
+		children.add(c);
+		br = new CFGBranch(graph);
+		br.use = stmt.cond.getNames();
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
+		CFGNode c = children.get(0);
+		NodeListPair pair = c.buildSubGraph();
+		NodeList entering = new NodeList(br);
+		NodeList exiting = new NodeList(pair.getRight());
+		br.succ.addAll(entering);
+		for (CFGNode i : entering) {
+				i.prev.add(br);
+			for (CFGNode j : exiting) {
+				j.succ.add(i);
+				i.prev.add(j);
+			}
+		}
+		return new NodeListPair(entering, exiting);
 	}
 
 	public StmtList translate() {
@@ -116,12 +180,46 @@ class CFGWhile extends CFGNode {
 
 class CFGCond extends CFGNode {
 	LCond stmt;
+	CFGBranch br;
 
 	public CFGCond (LCond s, CFG graph) {
 		stmt = s;
+		br = new CFGBranch(graph);
+		br.use = stmt.cond.getNames();
+		CFGNode c1 = stmt.ifBlock.toCFGNode(graph);
+		CFGNode c2 = stmt.elseBlock.toCFGNode(graph);
+		children.add(c1);
+		children.add(c2);
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
+		CFGNode c1 = children.get(0);
+		CFGNode c2 = children.get(1);
+
+		NodeListPair pair1 = c1.buildSubGraph();
+		NodeListPair pair2 = c2.buildSubGraph();
+
+		NodeList in1 = pair1.getLeft();
+		NodeList in2 = pair2.getLeft();
+		NodeList out1 = pair1.getRight();
+		NodeList out2 = pair2.getRight();
+
+		for (CFGNode i : in1) {
+			i.prev.add(br);
+			br.succ.add(i);
+		}
+
+		for (CFGNode i : in2) {
+			i.prev.add(br);
+			br.succ.add(i);
+		}
+
+		NodeList entering = new NodeList(br);
+		NodeList exiting = new NodeList(out1);
+		exiting.addAll(out2);
+
+
+		return new NodeListPair(entering, exiting);
 	}
 
 	public StmtList translate() {
@@ -129,15 +227,19 @@ class CFGCond extends CFGNode {
 	}
 }
 
-
 class CFGAssign extends CFGNode {
 	LAssign stmt;
 
 	public CFGAssign (LAssign s, CFG graph) {
 		stmt = s;
+		graph.nodes.add(this);
+		use = stmt.val.getNames();
+		def = stmt.var.getNames();
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
+		NodeList ret = new NodeList(this);
+		return new NodeListPair(ret, ret);
 	}
 
 	public StmtList translate() {
@@ -150,9 +252,29 @@ class CFGReturn extends CFGNode {
 
 	public CFGReturn (LReturn s, CFG graph) {
 		stmt = s;
+		graph.nodes.add(this);
+		use = stmt.ret.getNames();
 	}
 
-	public void setEdges (NodeList incoming, NodeList outgoing) {
+	public NodeListPair buildSubGraph () {
+		NodeList ret = new NodeList(this);
+		return new NodeListPair(ret, new NodeList());
+	}
+
+	public StmtList translate() {
+		return null;
+	}
+}
+
+class CFGBranch extends CFGNode {
+
+	public CFGBranch (CFG graph) {
+		graph.nodes.add(this);
+	}
+
+	public NodeListPair buildSubGraph () {
+		NodeList ret = new NodeList(this);
+		return new NodeListPair(ret, ret);
 	}
 
 	public StmtList translate() {
